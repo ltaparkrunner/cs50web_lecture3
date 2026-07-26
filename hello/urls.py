d635f88f7350d038ce25ab50c34a1efca9793abf
ltaparkrunner <ltaparkrunner@gmail.com>
2026-07-26 19:15:08 +0300

    Some new opportunities.

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("alex", views.alex, name="alex")
+    path("<str:name>", views.greet, name="greet"),
+    path("alex", views.alex, name="alex"),
+    path("tatiana", views.tatiana, name="tatiana")
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    # return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def alex(request):
     return HttpResponse("Hello, alex!")
+
+def tatiana(request):
+    return HttpResponse("Hello, Tatiana!")
+
+def greet(request, name):
+    # return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
